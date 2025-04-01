e93141a95e42fe82c5898971824526163ed558f1
Remy Siandja <remysiandja@gmail.com>
2025-04-02 00:19:06 +0100

    Ride alert model added and basic tests for ride model written

--- rides_and_requests/models.py
+++ rides_and_requests/models.py
@@ -23,7 +23,20 @@ class Ride(TimeStampedModel):
     arrival_town = models.CharField(max_length=100)
     departure_datetime = models.DateTimeField()
     available_seats = models.IntegerField(default=1, validators=[MinValueValidator(1, message="You cant create a ride with less that 1 seat available")])
-    expected_payment = models.FloatField()
+    price_per_seat = models.FloatField()
+    is_active = models.BooleanField(default=True)
+    
+    
+    
+class RideAlert(TimeStampedModel):
+    id = models.UUIDField(
+        default=uuid.uuid4, editable=False, primary_key=True, unique=True
+    )
+    user = models.ForeignKey(User, related_name="rides", on_delete=models.CASCADE())
+    departure_town = models.CharField(max_length=100)
+    arrival_town = models.CharField(max_length=100)
+    departure_datetime = models.DateTimeField()
+    available_seats = models.IntegerField(default=1, validators=[MinValueValidator(1, message="You cant create an alert for a ride with less that 1 seat available")])
     is_active = models.BooleanField(default=True)
     
     
@@ -36,7 +49,7 @@ class RideRequest(TimeStampedModel):
     arrival_town = models.CharField(max_length=100)
     departure_datetime = models.DateTimeField()
     required_seats = models.IntegerField(default=1, validators=[MinValueValidator(1, message="You cant create a request with less that one required seat")])
-    ride_payment = models.FloatField()
+    price_per_seat = models.FloatField()
     is_active = models.BooleanField(default=True)
 
 
---
+++ rides_and_requests/tests/__init__.py
@@ -0,0 +1,60 @@
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
+from requests import delete
+
+from rides_and_requests.models import Ride, RideRequest, RideAlert
+
+User = get_user_model()
+
+from django.utils import timezone
+from datetime import datetime, timedelta
+
+class RideModelTest(TestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email='modeltest@example.com',
+            username='modeltestuser',
+            password='testpassword',
+        )
+
+        
+        self.test_ride = Ride.objects.create(
+            user=self.user,
+            departure_town="Town A",
+            arrival_town="Town B",
+            departure_datetime=timezone.now(),
+            available_seats=3,
+            price_per_seat=1000,
+        )
+
+    def test_create_ride(self):
+        ride = Ride.objects.create(
+            user=self.user,
+            departure_town="Town A",
+            arrival_town="Town B",
+            departure_datetime=timezone.now(),
+            available_seats=3,
+            price_per_seat=1000,
+        )
+        
+        self.assertEqual(ride.user.username, "modeltestuser")
+        self.assertEqual(len(Ride.objects.all()), 2)
+        
+        
+    def test_update_ride(self):
+        self.test_ride.arrival_town = "Town C"
+        self.test_ride.price_per_seat = 700
+        self.test_ride.save()
+        
+        self.assertEqual(self.test_ride.arrival_town, "Town C")
+        self.assertEqual(self.test_ride.price_per_seat, 700)
+    
+    
+    def test_delete_ride(self):
+        self.test_ride.delete()
+        
+        self.assertEqual(len(Ride.objects.all()), 0)
+    
+    
