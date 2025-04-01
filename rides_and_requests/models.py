4eee3e44580c8321a0c83b102b8a00e5d83f36f6
Remy Siandja <remysiandja@gmail.com>
2025-04-01 23:44:01 +0100

    Project setup and creattion of the key models

---
+++ rides_and_requests/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+import uuid
+from django.core.validators import MinValueValidator
+
+User = get_user_model()
+
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    modeified_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+
+
+class Ride(TimeStampedModel):
+    id = models.UUIDField(
+        default=uuid.uuid4, editable=False, primary_key=True, unique=True
+    )
+    user = models.ForeignKey(User, related_name="rides", on_delete=models.CASCADE())
+    departure_town = models.CharField(max_length=100)
+    arrival_town = models.CharField(max_length=100)
+    departure_datetime = models.DateTimeField()
+    available_seats = models.IntegerField(default=1, validators=[MinValueValidator(1, message="You cant create a ride with less that 1 seat available")])
+    expected_payment = models.FloatField()
+    is_active = models.BooleanField(default=True)
+    
+    
+class RideRequest(TimeStampedModel):
+    id = models.UUIDField(
+        default=uuid.uuid4, editable=False, primary_key=True, unique=True
+    )
+    user = models.ForeignKey(User, related_name="ride_requests", on_delete=models.CASCADE())
+    departure_town = models.CharField(max_length=100)
+    arrival_town = models.CharField(max_length=100)
+    departure_datetime = models.DateTimeField()
+    required_seats = models.IntegerField(default=1, validators=[MinValueValidator(1, message="You cant create a request with less that one required seat")])
+    ride_payment = models.FloatField()
+    is_active = models.BooleanField(default=True)
+
+
+
