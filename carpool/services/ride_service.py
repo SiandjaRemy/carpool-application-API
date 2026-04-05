8c6bb591fd5e5622f98754bd4a3f87882a134c24
Remy Siandja <remysiandja@gmail.com>
2026-04-05 11:25:15 -0400

    feat:  backgroud tasks created using the ride batch service created

--- carpool/services/ride_service.py
+++ carpool/services/ride_service.py
@@ -4,15 +4,12 @@ import logging
 from django.db import transaction
 from django.utils import timezone
 
-from carpool.exceptions import (
-    BusinessValidationError,
-    ResourceNotFoundError,
-    ResourcePermissionError,
-)
+from carpool.exceptions import BusinessValidationError, ResourcePermissionError
 from carpool.models import Ride, RideRequest
 from carpool.enums.enums import RequestStatus, ReservationPaymentStatus, RideStatus
 
 from carpool.models.reservation import Reservation
+from carpool.send_emails import alert_a_user_via_email
 from carpool.services.request_service import RideRequestService
 
 celery_logger = logging.getLogger("celery")
@@ -162,6 +159,53 @@ class RideBatchService:
         ).update(status=RequestStatus.EXPIRED, updated_at=timezone.now())
 
         return f"Started {count} rides and expired associated pending requests."
+
+    @classmethod
+    @transaction.atomic
+    def auto_start_rides_2(cls):
+        # Calculate threshold time (10 minutes from now)
+        threshold_time = timezone.now() + timedelta(minutes=10)
+
+        # Get rides with status SCHEDULED AND departure time <= threshold
+        upcoming_rides = (
+            Ride.objects.select_related("user")
+            .filter(status=RideStatus.SCHEDULED, departure_datetime__lte=threshold_time)
+            .order_by("-created_at")
+        )
+
+        if upcoming_rides.exists():
+            id_list = [ride.id for ride in upcoming_rides]
+            # Update rides in bulk
+            Ride.objects.filter(id__in=id_list).update()
+
+            # Send emails for this expired_rides
+            for ride in upcoming_rides:
+                subject = f"Your ride from {ride.departure_town} to {ride.arrival_town} is approaching"
+                message = (
+                    f"Hey {ride.user.username},\n\n"
+                    f"This is a reminder that your ride from {ride.departure_town} "
+                    f"to {ride.arrival_town} is scheduled to depart within the next 10 minutes.\n\n"
+                    f"Departure Town: {ride.departure_town}\n"
+                    f"Arrival Town: {ride.arrival_town}\n"
+                    f"Departure Time: {ride.departure_datetime.strftime('%Y-%m-%d %H:%M')}\n\n"
+                    f"The ride will be marked as inactive shortly."
+                )
+
+                try:
+                    # Comment when working offline
+                    alert_a_user_via_email(
+                        email=ride.user.email, subject=subject, message=message
+                    )
+                    print(message)
+                except Exception as e:
+                    celery_logger.error(
+                        f"Failed to send notification for ride {ride.id}: {str(e)}"
+                    )
+
+        else:
+            return "No expired rides found"
+
+        return f"Deactivated {len(upcoming_rides)} expired rides"
 
     @classmethod
     def auto_expire_past_rides(cls):
--- carpool/tasks/ride_tasks.py
+++ carpool/tasks/ride_tasks.py
@@ -1,61 +1,26 @@
 from celery import shared_task
-
-from django.utils import timezone
-from datetime import timedelta
-
-import logging
-
-from carpool.enums.enums import RideStatus
-from carpool.models.ride import Ride
-from carpool.send_emails import alert_a_user_via_email
+from carpool.services.ride_service import RideBatchService
 
 
-logger = logging.getLogger("background")
+@shared_task
+def task_auto_start_rides():
+    result = RideBatchService.auto_start_rides()
+    return result
 
 
-# A periodic task that will be triggered every 5 minutes and will update the status of all instances with dates in the next 10 minutes
-@shared_task(name="rides.tasks.update_ride_statuses")
-def update_ride_statuses():
-    # Calculate threshold time (10 minutes from now)
-    threshold_time = timezone.now() + timedelta(minutes=10)
+@shared_task
+def task_auto_ride_departure_reminder():
+    result = RideBatchService.send_departure_reminders()
+    return result
 
-    # Get rides with status SCHEDULED AND departure time <= threshold
-    upcoming_rides = (
-        Ride.objects.select_related("user")
-        .filter(status=RideStatus.SCHEDULED, departure_datetime__lte=threshold_time)
-        .order_by("-created_at")
-    )
 
-    if upcoming_rides.exists():
-        id_list = [ride.id for ride in upcoming_rides]
-        # Update rides in bulk
-        Ride.objects.filter(id__in=id_list).update()
+@shared_task
+def task_auto_remind_unpaid_reservations():
+    result = RideBatchService.remind_unpaid_reservations()
+    return result
 
-        # Send emails for this expired_rides
-        for ride in upcoming_rides:
-            subject = f"Your ride from {ride.departure_town} to {ride.arrival_town} is approaching"
-            message = (
-                f"Hey {ride.user.username},\n\n"
-                f"This is a reminder that your ride from {ride.departure_town} "
-                f"to {ride.arrival_town} is scheduled to depart within the next 10 minutes.\n\n"
-                f"Departure Town: {ride.departure_town}\n"
-                f"Arrival Town: {ride.arrival_town}\n"
-                f"Departure Time: {ride.departure_datetime.strftime('%Y-%m-%d %H:%M')}\n\n"
-                f"The ride will be marked as inactive shortly."
-            )
 
-            try:
-                # Comment when working offline
-                alert_a_user_via_email(
-                    email=ride.user.email, subject=subject, message=message
-                )
-                print(message)
-            except Exception as e:
-                logger.error(
-                    f"Failed to send notification for ride {ride.id}: {str(e)}"
-                )
-
-    else:
-        return "No expired rides found"
-
-    return f"Deactivated {len(upcoming_rides)} expired rides"
+@shared_task
+def task_auto_request_reviews():
+    result = RideBatchService.request_review_reminders()
+    return result
